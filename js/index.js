56829aa663c5951b9d87c07ee32da1842e952fa4
Alan Lo <alan-lo@users.noreply.github.com>
2017-02-26 09:10:26 -0800

    Fix jumbotron button hovering

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(`a[href="#projects"]:nth-child(1) > div.down-arrow`).on('mouseenter', function( event){
+        $(this).addClass('down-arrow-circle-hover')
+        $('a[href="#projects"]:nth-child(1) i.fa.fa-angle-down.fa-3x').addClass('down-arrow-hover')
+
+    });
+    $(`a[href="#projects"]:nth-child(1) > div.down-arrow`).on('mouseleave', function( event){
+        $(this).removeClass('down-arrow-circle-hover')
+        $('a[href="#projects"]:nth-child(1) i.fa.fa-angle-down.fa-3x').removeClass('down-arrow-hover')
+    });
+});
